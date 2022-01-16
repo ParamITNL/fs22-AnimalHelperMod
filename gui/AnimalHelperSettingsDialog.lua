5d017f0ee6a5d796c48adc851405615c4674cd32
Peter van Etten <peter.vanEtten@cito.nl>
2022-01-16 23:42:30 +0100

    First field almost working

--- gui/AnimalHelperSettingsDialog.lua
+++ gui/AnimalHelperSettingsDialog.lua
@@ -3,19 +3,35 @@ local AnimalHelperSettingsDialog_mt = Class(AnimalHelperSettingsDialog, DialogEl
 
 AnimalHelperSettingsDialog.CONTROLS = {
     SAVE_BUTTON = "okButton",
-	BACK_BUTTON = "backButton"
+	BACK_BUTTON = "backButton",
+	START_HOUR = "helperStartHourElement"
 }
 
-function AnimalHelperSettingsDialog.new(target, customMt)
+function AnimalHelperSettingsDialog.new(target, customMt, l10n)
     local self = DialogElement.new(target, customMt or AnimalHelperSettingsDialog_mt)
+	self.l10n = l10n
+	self.workerStartHours = {}
+	for i = 8, 23, 1 do
+		table.insert(self.workerStartHours, string.format("%02d:00", i))
+	end
+
     self:registerControls(AnimalHelperSettingsDialog.CONTROLS)
+
     return self
 end
 
 function AnimalHelperSettingsDialog:onOpen()
 	AnimalHelperSettingsDialog:superClass().onOpen(self)
+	local workHour = 0
 
-	self:initValues(true)
+	for i = 1, table.getn(self.workerStartHours) do
+		if string.format("%02d:00", AnimalHelper.startHour) == self.workerStartHours[i] then
+			workHour = i
+			break
+		end
+	end
+
+	self.helperStartHourElement:setState(workHour)
 
 	-- local dynInfo = g_currentMission.missionDynamicInfo
 	-- local numPlayers = dynInfo.capacity
@@ -39,18 +55,16 @@ function AnimalHelperSettingsDialog:onOpen()
 -- 	self.boxLayout:invalidateLayout()
 end
 
-function AnimalHelperSettingsDialog:initValues(force)
-
+function AnimalHelperSettingsDialog:onCreateStartHour(element)
+	element:setTexts(self.workerStartHours)
+	element:setState(table.getn(self.workerStartHours))
 end
 
 function AnimalHelperSettingsDialog:onClickOk()
 	print("OK CLICKED")
-	local workerStartHour = self.startHourElement:getText()
-	if MathUtil.IsNaN(workerStartHour) then
-		return false
-	end
+	local workerStartHour = self.helperStartHourElement:getState()
 
-	printdbg("Worker Start Hour '%02d:00'")
+	printdbg("Worker Start Hour '%s'", workerStartHour)
     -- 	local password = self.passwordElement:getText()
     -- 	local capacity = self.capacityNumberTable[self.capacityElement:getState()]
     -- 	local autoAccept = self.autoAcceptElement:getIsChecked()
